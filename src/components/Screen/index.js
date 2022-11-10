b9f0467b1d928ea0956134acc058517699b310c0
maddumajohnerick <madduma.johnerick@gmail.com>
2022-11-10 23:43:38 +0800

    menu bar

--- src/components/Screen/index.js
+++ src/components/Screen/index.js
@@ -6,6 +6,9 @@ import {
   VscSync,
   VscWarning,
   VscError,
+  VscClose,
+  VscChromeRestore,
+  VscChromeMinimize,
 } from 'react-icons/vsc';
 import './Screen.scss';
 
@@ -13,8 +16,34 @@ const Screen = ({}) => {
   return (
     <div className="Screen" style={{ backgroundImage: `url('/galaxy.jpg')` }}>
       <div className="vscode">
-        <div className="menubar"></div>
+        <div className="menubar">
+          <div className="left">
+            <img alt="vscode" src="/vscode.png" />
+            <div className="button opt1">File</div>
+            <div className="button opt2">Edit</div>
+            <div className="button opt3">Selection</div>
+            <div className="button opt4">View</div>
+            <div className="button opt5">Go</div>
+            <div className="button opt6">Run</div>
+            <div className="button opt7">Terminal</div>
+            <div className="button opt8">Help</div>
+            <div className="button optn">Options</div>
+          </div>
+          <div className="right">
+            <div className="button ">
+              <VscChromeMinimize size={22} />
+            </div>
+            <div className="button ">
+              <VscChromeRestore size={22} />
+            </div>
+            <div className="button close">
+              <VscClose size={22} />
+            </div>
+          </div>
+        </div>
+
         <div className="contents"></div>
+
         <div className="botbar">
           <div className="left">
             <VscSourceControl size={16} />
