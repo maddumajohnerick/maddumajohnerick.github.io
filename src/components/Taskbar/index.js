46cfc59233ad055c9b50efc0bded30de3ba1f4d5
Erick Madduma <emadduma@Arcanys.com>
2022-11-10 20:17:04 +0800

    left side

--- src/components/Taskbar/index.js
+++ src/components/Taskbar/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { AiFillWindows } from 'react-icons/ai';
+import { AiFillWindows, AiOutlineWifi } from 'react-icons/ai';
+import { RiMessageFill } from 'react-icons/ri';
+import { CiBatteryFull } from 'react-icons/ci';
+import { GiSpeaker } from 'react-icons/gi';
 import './Taskbar.scss';
 
 const Taskbar = ({}) => {
@@ -9,15 +12,28 @@ const Taskbar = ({}) => {
         <div className="windows">
           <AiFillWindows size={25} />
         </div>
+        <div className="vscode">
+          <img alt="vscode" src="/vscode.png" />
+        </div>
       </div>
       <div className="task-right">
-        <img src="/task-icons.png" alt="task-icons" className="task-icons" />
+        <div className="batt">
+          <CiBatteryFull size={21} />
+        </div>
+        <div className="wifi">
+          <AiOutlineWifi size={20} />
+        </div>
+        <div className="speaker">
+          <GiSpeaker size={23} />
+        </div>
         <div className="lang">ENG</div>
-        <div>
+        <div className="time">
           <div>10:30 pm</div>
           <div>10/11/2022</div>
         </div>
-        <img src="/tooltip.png" alt="tooltip" className="tooltip" />
+        <div className="tooltip">
+          <RiMessageFill size={22} />
+        </div>
       </div>
     </div>
   );
